cc95e78b6523c138b61c9231e7d769cc7552a675
Vladimir Krutkin <krutkinvs@gmail.com>
2024-09-30 18:30:08 +0300

    Login page improvements

--- examples/chat/src/Pages/Login.hs
+++ examples/chat/src/Pages/Login.hs
@@ -14,30 +14,40 @@ import Web.Scotty.Trans
 
 loginPage :: ActionT AppM ()
 loginPage = do
-    c <- lift $ asks config
-    let sigils' = sigils c
-    html $ renderText $ do
-        doctype_
-        html_ [lang_ "en"] $ do
-            htmlHead
-            htmlBody sigils'
+  c <- lift $ asks config
+  let sigils' = sigils c
+  html $ renderText $ do
+    doctype_
+    html_ [lang_ "en"] $ do
+      htmlHead
+      htmlBody sigils'
 
 htmlBody :: [MySigil] -> Html ()
 htmlBody sigils' = body_ [class_ "min-h-screen flex"] $
-    div_ [class_ "container grow flex flex-col"] $ do
-        div_ [class_ "content-header header-color"] $ do
-            div_ "hbs2 chat"
-            div_ [class_ "header-buttons"] $ do
-                themeToggleButton
-        div_ [class_ "grow login-wrapper"] $ do
-            div_ $ do
-                h2_ "Select user"
-                div_ [role_ "group"] $ do
-                    select_ [name_ "sigil", id_ "user-select", ariaLabel_ "Select user", required_ ""] $ do
-                        forM_ sigils' $ \sigil ->
-                            let sigilText = Text.pack $ show $ pretty sigil
-                             in option_ [value_ sigilText] $ toHtml sigilText
-                    button_ [class_ "whitespace-nowrap", handleLogin] "Log in"
+  div_ [class_ "container grow flex flex-col"] $ do
+    div_ [class_ "content-header header-color"] $ do
+      div_ "hbs2 chat"
+      div_ [class_ "header-buttons"] $ do
+        themeToggleButton
+    div_ [class_ "grow login-wrapper"] $ do
+      div_ $ do
+        case sigils' of
+          [] -> noSigilsMessage
+          someSigils -> do
+            h2_ "Select user"
+            div_ [role_ "group"] $ do
+              select_ [name_ "sigil", id_ "user-select", ariaLabel_ "Select user", required_ ""] $ do
+                forM_ someSigils $ \sigil ->
+                  let sigilText = Text.pack $ show $ pretty sigil
+                   in option_ [value_ sigilText] $ toHtml sigilText
+              button_ [class_ "whitespace-nowrap", handleLogin] "Log in"
+
+noSigilsMessage :: Html ()
+noSigilsMessage = do
+  h2_ "No sigils available"
+  p_ $ do
+    "You can specify the sigils you want to use in the configuration file located at "
+    code_ "~/.config/hbs2-chat/config"
 
 handleLogin :: Attribute
 handleLogin = hyper_ "on click set localStorage.user to #user-select.value then go to url '/'"
